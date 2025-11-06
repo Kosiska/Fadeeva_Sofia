be526a76cdac327c6afb54c7cf878805774e9e39
Kosiska <164752888+Kosiska@users.noreply.github.com>
2025-11-07 01:54:30 +0300

    Add files via upload

--- practice/pract02/pract2_1.cpp
+++ practice/pract02/pract2_1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <windows.h>
 #include <ctime>
-using namespace std; //РІС‹Р±РѕСЂ РІСЃС‚Р°РІРєРё РїСѓР·С‹СЂРµРє
+using namespace std; //выбор вставки пузырек
 
-void random_fill(int s[], int n, int start, int end){ //С„СѓРЅРєС†РёСЏ Р·Р°РїРѕР»РЅРµРЅРёСЏ s[] СЃР»СѓС‡Р°Р№РЅС‹РјРё С‡РёСЃР»Р°РјРё
+void random_fill(int s[], int n, int start, int end){ //функция заполнения s[] случайными числами
     srand(time(0));
     for (int i = 0; i < n; i ++){
         int x = rand() % (end - start + 1) + start;
@@ -10,7 +11,7 @@ void random_fill(int s[], int n, int start, int end){ //С„СѓРЅРєС†РёСЏ Р·Р°РїРѕР
     }
 }
 
-void vivo(int s[], int n){ //РІС‹РІРѕРґ РјР°СЃСЃРёРІР°
+void vivo(int s[], int n){ //вывод массива
 
      for (int i = 0; i < n; i++){
         cout << s[i] << " ";
@@ -18,7 +19,7 @@ void vivo(int s[], int n){ //РІС‹РІРѕРґ РјР°СЃСЃРёРІР°
     cout << endl;
 }
 
-void search(int s[], int n){ //СЃРѕСЂС‚РёСЂРѕРІРєР° РІС‹Р±РѕСЂРѕРј (РїРѕРёСЃРєРѕРј)
+void search(int s[], int n){ //сортировка выбором (поиском)
     for (int i = 0; i < n; i++){
         int jmin = i;
         for (int j = i + 1; j < n; j++){
@@ -31,15 +32,24 @@ void search(int s[], int n){ //СЃРѕСЂС‚РёСЂРѕРІРєР° РІС‹Р±РѕСЂРѕРј (РїРѕРёСЃРєРѕ
 
 }
 
-void insert(int s[], int n){
+
+void insert(int s[], int n){ //сортировка вставкой
     for (int i = 1; i < n; i++){
-        for (int j = i; j < 0 && s[j] < s[j-1]; j--){
-            swap(s[j], s[j-1]);
+        int tmp = s[i];
+        int j = i;
+
+        while (j > 0 && tmp < s[j-1]){
+            s[j] = s[j-1];
+            j--;
         }
+        s[j] = tmp;
     }
 }
 
-void bubble(int s[], int n){
+
+
+
+void bubble(int s[], int n){ //сортировка пузырьком
     bool f = true;
     int j = 0;
     while(f){
@@ -56,11 +66,11 @@ void bubble(int s[], int n){
 
 int main(){
     setlocale(0, "");
-    int n = 100, start = 0, end = 10000;
+    int n = 100, start = 0, end = 9999;
     int* s = new int[n];
-    cout << "Р”Рѕ Р·Р°РїРѕР»РЅРµРЅРёСЏ РјР°СЃСЃРёРІР° С‡РёСЃР»Р°РјРё:" << endl;
+    cout << "До заполнения массива числами:" << endl;
     vivo(s, n);
-    cout << endl << "РџРѕСЃР»Рµ Р·Р°РїРѕР»РЅРµРЅРёСЏ СЃР»СѓС‡Р°Р№РЅС‹РјРё С‡РёСЃР»Р°РјРё: " << endl;;
+    cout << endl << "После заполнения случайными числами: " << endl;;
     random_fill(s, n, start, end);
     vivo(s, n);
 
